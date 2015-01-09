2a5402bbc40127c890c521da37ff7a53810d6702
Lolla Manolla <lollamanolla@gmail.com>
2015-01-09 01:28:03 +0100

    js

--- js/app.js
+++ js/app.js
@@ -2,51 +2,46 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+  //Scrolling 
+  $(document).ready(
+  function() {  
+    $("html").niceScroll();
     }
-}());
+  );  
 
+  //Navigation  
+$('#navigation').slimmenu(
+{
+    resizeWidth: '800',
+    collapserTitle: '',
+    animSpeed: 'medium',
+    easingEffect: 'easeInSine',
+    indentChildren: false,
+    childrenIndenter:'&raquo;'
 
-//scroll to 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
+});
+
+//Home fit screen 
+  /*global $:false */
+  $(function(){"use strict";
+    $('#home').css({'height':($(window).height())+'px'});
+    $(window).resize(function(){
+    $('#home').css({'height':($(window).height())+'px'});
+    });
+  });
+
+/*global $:false */
+$(document).ready(function(){"use strict";
+  $(".scroll").click(function(event){
+
+    event.preventDefault();
+
+    var full_url = this.href;
+    var parts = full_url.split("#");
+    var trgt = parts[1];
+    var target_offset = $("#"+trgt).offset();
+    var target_top = target_offset.top - 60;
+
+    $('html, body').animate({scrollTop:target_top}, 1200);
   });
 });
-
-//Home fit screen
-/*global $:false */
-$(function(){"use strict";
-$('#home').css({'height':($(window).height())+'px'});
-$(window).resize(function(){
-$('#home').css({'height':($(window).height())+'px'});
-});
-}); 
